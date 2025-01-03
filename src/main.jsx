6e439005f05d2b877714361b6668ea79a0b69f71
tusherbhakta <tusheranta@gmail.com>
2025-01-03 18:05:18 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,38 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from './App.jsx'
+import Home from './components/Home.jsx';
+import Login from './components/Login.jsx';
+import SignUp from './components/SignUp.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children:[
+      {
+        path: '/',
+        element:<Home></Home>
+      },
+      {
+        path: 'login',
+        element: <Login></Login>
+      },
+      {
+        path: 'signup',
+        element: <SignUp></SignUp>
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
