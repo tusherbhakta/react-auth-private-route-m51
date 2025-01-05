8ad11b584f3f72b8a6c63237c4945e5fd89356be
tusherbhakta <tusheranta@gmail.com>
2025-01-05 23:28:44 +0530

    some fuctionality added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 const Login = () => {
+    const handleLogin = e => {
+        e.preventDefault(); 
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+    }
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col">
@@ -8,18 +14,18 @@ const Login = () => {
                     <h1 className="text-5xl mb-3 font-bold">Login now!</h1>
                 </div>
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                    <form className="card-body">
+                    <form onSubmit={handleLogin} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" placeholder="email" className="input input-bordered" required />
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" placeholder="password" className="input input-bordered" required />
+                            <input type="password"  name="password"  placeholder="password" className="input input-bordered" required />
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
