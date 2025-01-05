fb295418ad3735fa18a737ff9ba91250e7d431d8
tusherbhakta <tusheranta@gmail.com>
2025-01-06 00:00:02 +0530

    add

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -13,6 +13,12 @@ const SignUp = () => {
                 </div>
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                     <form onSubmit={handleSignup} className="card-body">
+                    <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name="name" placeholder="Name" className="input input-bordered" required />
+                        </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
